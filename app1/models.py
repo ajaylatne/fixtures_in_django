ec6a133b823708d784eff3e5c5290d0fb3492070
ajaylatne <121288483+ajaylatne@users.noreply.github.com>
2024-01-22 12:34:44 +0530

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# python manage.py loaddata student_data.json
+# python manage.py loaddata student_data.json  --> to pass data from fixture file to db
+
+# py manage.py dumpdata app1.Student --output=app1/fixtures/student_data.json --> to fetch data from db and add into fixture file
 
 
 class Student(models.Model):
